6ce3724f51beb16cc1ad9f088468242610496146
Subochev <caffeine.mgn@gmail.com>
2017-10-26 17:44:45 +0300

    fix: style declaration

--- README.md
+++ README.md
@@ -1,14 +1,2 @@
-# css
-Использование:
-apply plugin: "jsResourceBuilder"
-
-buildResource {
-    def imageFolder = "$projectDir/images"
-    outFile = file("$out/resources.js")
-    css {
-        images {
-            add("checked_png", file("$imageFolder/checked.png"))
-            add("checked_hover_png", file("$imageFolder/checked_hover.png"))
-        }
-    }
-}
+# CSS
+Kotlin library for CSS
--- examples/src/main/kotlin/org/tlsys/examples/Main.kt
+++ examples/src/main/kotlin/org/tlsys/examples/Main.kt
@@ -4,57 +4,57 @@ import org.tlsys.css.CSS
 import org.w3c.dom.HTMLButtonElement
 import kotlin.browser.document
 
-
+private const val BTN_STYLE = "toggleStyle"
 val css = CSS {
-        "name" then {
+    ".$BTN_STYLE" then {
+        backgroundColor = "#red"
+    }
 
+    val baseBtn = template {
+        padding = "3px 16px 3px 16px"
+        borderRadius = "4px"
+        /*
+        ":active" then {
+            top = "1px"
+            left = "1px"
+            position = "relative"
         }
+        */
+    }
 
-        !"name"{
+    ".confirmBtn" {
+        extend(baseBtn)
+        backgroundColor = "#8cc152"
+        border = "1px solid #8cc152"
+        color = "#fff"
+    }
 
-        }
-        val baseBtn = template {
-            padding = "3px 16px 3px 16px"
-            borderRadius = "4px"
-            /*
-            then(":active") {
-                top = "1px"
-                left = "1px"
-                position = "relative"
+    ".cancelBtn" {
+        extend(baseBtn)
+        border = "1px solid #a9b2bd"
+        backgroundColor = "#fff"
+        color = "#000"
+    }
+
+    ".d1" {
+        border = "solid 1px"
+        ".p2" {
+            color = "black"
+
+            ">.userName" then {
+                color = "red"
             }
-            */
-        }
-
-        add(".confirmBtn") {
-            extend(baseBtn)
-            backgroundColor = "#8cc152"
-            border = "1px solid #8cc152"
-            color = "#fff"
-        }
-
-        add(".cancelBtn") {
-            extend(baseBtn)
-            border = "1px solid #a9b2bd"
-            backgroundColor = "#fff"
-            color = "#000"
-        }
-
-        add(".d1") {
-            border = "solid 1px"
-            add(".p2") {
-                color = "black"
-
-                ">.userName" then {
-                    color = "red"
-                }
-                hover {
-                    color = "green"
-                }
+            hover {
+                color = "green"
             }
         }
     }
+}
 
 fun main(args: Array<String>) {
 
     val btn = (document.getElementById("btn") as HTMLButtonElement)
+    btn.onclick = {
+        btn.classList.toggle(BTN_STYLE)
+    }
 }
